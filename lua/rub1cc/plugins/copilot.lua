ba3bcda3c9ad94a400479ad2478499b4712bd1af
Faraz Aulia <hey@rub1.cc>
2025-03-24 06:46:06 +0700

    add copilot chat

--- lua/rub1cc/plugins/copilot.lua
+++ lua/rub1cc/plugins/copilot.lua
@@ -4,4 +4,14 @@ return {
   config = function()
     vim.g.copilot_node_command = '~/.nvm/versions/node/v20.10.0/bin/node'
   end,
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+    { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+  },
+  build = 'make tiktoken', -- Only on MacOS or Linux
+  opts = {
+    -- See Configuration section for options
+  },
+  -- See Commands section for default commands if you want to lazy load on them
 }
