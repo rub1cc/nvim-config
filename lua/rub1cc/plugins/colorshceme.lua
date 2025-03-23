138fb3346055448abc55457908fefd54ad44ec92
Faraz Aulia <hey@rub1.cc>
2025-03-24 06:21:38 +0700

    update color scheme

--- lua/rub1cc/plugins/colorshceme.lua
+++ lua/rub1cc/plugins/colorshceme.lua
@@ -6,15 +6,22 @@ return {
   --   vim.cmd.colorscheme 'nordic'
   -- end,
 
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
+  -- 'folke/tokyonight.nvim',
+  -- priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- init = function()
+  --   -- Load the colorscheme here.
+  --   -- Like many other themes, this one has different styles, and you could load
+  --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --   vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --   -- You can configure highlights by doing something like:
+  --   vim.cmd.hi 'Comment gui=none'
+  -- end,
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+  'https://github.com/ceigh/vercel-theme.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd 'colorscheme vercel'
   end,
 }
